5308e0cce84ef0c972487e1a5635ed68c33e99d4
YJ <Yujung2023@naver.com>
2025-11-18 17:40:17 +0900

    app.js modify

--- src/App.js
+++ src/App.js
@@ -8,19 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-
-        {/* 네비게이션 */}
-        <nav style={{ marginBottom: "20px" }}>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/personalColor">Personal Color</Link>|{" "}
-          <Link to="/fitroom">FitRoom</Link>
-        </nav>
-
         <Routes>
-          {/* 홈은 ContentMain 사용 */}
           <Route path="/" element={<ContentMain />} />
-
-          {/* 퍼스널 컬러 페이지 */}
           <Route path="/personalColor" element={<PersonalColor />} />
           <Route path="/fitroom/*" element={<FitRoom />} />
         </Routes>
